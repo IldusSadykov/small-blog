84e948f0fe9be594bf858e09fac58e6250f854a4
Ildus Sadykov <ildus.sadykov@flatstack.com>
2016-07-12 10:21:35 +0300

    add customer to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711232501) do
+ActiveRecord::Schema.define(version: 20160712061103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -150,6 +150,7 @@ ActiveRecord::Schema.define(version: 20160711232501) do
     t.datetime "updated_at"
     t.string   "full_name"
     t.uuid     "location_id"
+    t.uuid     "customer_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
